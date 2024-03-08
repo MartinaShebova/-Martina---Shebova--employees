161250f690e8fe1300e6cf0d3a6f8ff27615b940
Martinsita <martina2314@gmail.com>
2024-03-08 12:50:53 +0200

    Added table and styling

--- components/commonProjectsDataGrid/commonProjectsDataGrid.jsx
+++ components/commonProjectsDataGrid/commonProjectsDataGrid.jsx
@@ -1,14 +1,46 @@
-function CommonProjectsDataGrid({ dataGridData }){
-    function containsScript(row) {
-        for (const cell of row) {
-            if (/<script[\s\S]*?>[\s\S]*?<\/script>/i.test(cell)) {
-                return true;
-            }
-        }
-        return false;
-    }
+import styles from '../../styles/CommonProjectsDataGrid.module.css';
+
+function CommonProjectsDataGrid({ dataGridData }) {
 
     console.log("dataGridData", dataGridData);
+    // {
+    //     114-218: 434
+    // }
+
+    const extractValues = (data) => {
+        return (
+            <div>
+                {Object.keys(dataGridData).map(key => (
+                    <td key={key}>{key}: {data[key]}</td>
+                ))}
+            </div>
+        );
+    }
+
+    return <>
+        <table className={styles.table}>
+            <thead>
+                <tr>
+                    <th className={styles.header}>Employee ID #1</th>
+                    <th className={styles.header}>Employee ID #2</th>
+                    <th className={styles.header}>Days Worked</th>
+                </tr>
+            </thead>
+            <tbody>
+                {Object.keys(dataGridData).map((key, index) => {
+                    const pairs = key.split("-");
+                    const firstEmployee = pairs[0];
+                    const secondEmployee = pairs[1];
+
+                    return <tr key={index} className={styles.row}>
+                        <td className={styles.cell}>{firstEmployee}</td>
+                        <td className={styles.cell}>{secondEmployee}</td>
+                        <td className={styles.cell}>{dataGridData[key]}</td>
+                    </tr>
+                })}
+            </tbody>
+        </table>
+    </>
 }
 
 export default CommonProjectsDataGrid;
--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<main className={`${styles.main} ${inter.className}`}>
+				<h2 className={styles.heading}>Pair of employees who have worked together</h2>
 				<EmployeesWorkedTogetherWidget />
 			</main>
 		</>
